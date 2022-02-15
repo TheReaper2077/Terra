670d51fff8cd94899296c806c4776f37b66685c6
TheSuperKDK <sreenikalarikkal@gmail.com>
2022-02-15 22:52:06 +0530

    shader optimisation

--- src/render/Types.h
+++ src/render/Types.h
@@ -1,25 +1,19 @@
 #pragma once
 
-#define LAYOUT {3, 4, 2, 1}
+#define LAYOUT {3, 4}
 
 struct Vertex2D {
 	float pos[3];
 	float color[4];
-	float tex[2];
-	float tex_index;
 
 	Vertex2D(float x, float y, float *color, float tx, float ty, float tex_index) {
 		setPos(x, y, 0);
-		setTex(tx, ty);
-		setColor(color);
-		this->tex_index = tex_index;
+		setColor(tx, ty, tex_index, 0);
 	}
 
 	Vertex2D(float x, float y, float z, float *color, float tx, float ty, float tex_index) {
 		setPos(x, y, z);
-		setTex(tx, ty);
-		setColor(color);
-		this->tex_index = tex_index;
+		setColor(tx, ty, tex_index, 0);
 	}
 
 	Vertex2D(float x, float y, float *color) {
@@ -37,11 +31,6 @@ private:
 		pos[0] = x;
 		pos[1] = y;
 		pos[2] = z;
-	}
-
-	void setTex(float x, float y) {
-		tex[0] = x;
-		tex[1] = y;
 	}
 
 	void setColor(float r, float g, float b, float a) {
--- src/render/shaders/texture.vs
+++ src/render/shaders/texture.vs
@@ -2,8 +2,8 @@
 
 layout (location = 0) in vec4 v_Position;
 layout (location = 1) in vec4 v_Color;
-layout (location = 2) in vec2 v_TexCoord;
-layout (location = 3) in float v_TextureIndex;
+// layout (location = 2) in vec2 v_TexCoord;
+// layout (location = 3) in float v_TextureIndex;
 
 layout (std140) uniform Matrices {
     mat4 u_Projection;
@@ -13,11 +13,11 @@ layout (std140) uniform Matrices {
 
 out float f_TextureIndex;
 out vec2 f_TexCoord;
-out vec4 f_Color;
+// out vec4 f_Color;
 
 void main() {
 	gl_Position = u_Projection * u_View * u_Model * v_Position;
-	f_Color = v_Color;
-	f_TextureIndex = v_TextureIndex;
-	f_TexCoord = v_TexCoord;
+	// f_Color = v_Color;
+	f_TextureIndex = v_Color.z;
+	f_TexCoord = v_Color.xy;
 }
